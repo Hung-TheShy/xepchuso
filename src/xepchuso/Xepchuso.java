0171d99d79ab2743a282d58e3bcc94b51f57f874
DELL <DELL@DESKTOP-OGRVV23>
2021-10-06 14:26:22 +0700

    xepchuso

---
+++ src/xepchuso/Xepchuso.java
@@ -0,0 +1,22 @@
+
+package xepchuso;
+
+
+public class Xepchuso {
+
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    
+
+ 
+    
+   
+    }
+    
+}
